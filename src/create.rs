5c8c8a3ae4b3494dd42613227873257ffc937305
nathanfaucett <nathanfaucett@gmail.com>
2016-04-13 19:41:09 -0500

    added new

--- src/create.rs
+++ src/create.rs
@@ -2,17 +2,19 @@ use num::Num;
 
 
 #[inline(always)]
-pub fn create<T: Num>(x: T, y: T, z: T) -> [T; 3] {[x, y, z]}
+pub fn new<T: Num>(x: T, y: T, z: T) -> [T; 3] {[x, y, z]}
+#[inline(always)]
+pub fn create<T: Num>(x: T, y: T, z: T) -> [T; 3] {new(x, y, z)}
 #[test]
-fn test_create() {
-    let v = create(1, 2, 3);
+fn test_new() {
+    let v = new(1, 2, 3);
     assert!(v[0] == 1);
     assert!(v[1] == 2);
     assert!(v[2] == 3);
 }
 
 #[inline(always)]
-pub fn clone<T: Num>(v: [T; 3]) -> [T; 3] {create(v[0], v[1], v[2])}
+pub fn clone<T: Num>(v: [T; 3]) -> [T; 3] {new(v[0], v[1], v[2])}
 
 #[inline(always)]
 pub fn copy<T: Num>(out: &mut [T; 3], a: [T; 3]) -> &mut [T; 3] {
