cbcf695c34f537e7903127587bc6d6d25611c357
nathanfaucett <nathanfaucett@gmail.com>
2016-11-14 14:19:57 -0600

    clean up

--- src/create.rs
+++ src/create.rs
@@ -29,3 +29,17 @@ fn test_copy() {
     copy(&mut v, &[1, 2, 3]);
     assert!(v == [1, 2, 3]);
 }
+
+#[inline(always)]
+pub fn from_vec2<'a, 'b, T: Num>(out: &'a mut [T; 3], v: &'b [T; 2]) -> &'a mut [T; 3] {
+    out[0] = v[0];
+    out[1] = v[1];
+    out
+}
+#[inline(always)]
+pub fn from_vec4<'a, 'b, T: Num>(out: &'a mut [T; 3], v: &'b [T; 4]) -> &'a mut [T; 3] {
+    out[0] = v[0];
+    out[1] = v[1];
+    out[2] = v[2];
+    out
+}
